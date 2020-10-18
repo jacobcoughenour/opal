e520259c37968941a3722b13aa4bd99495cb9610
Jacob Coughenour <jacob15545@yahoo.com>
2020-10-18 01:57:21 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <vulkan/vulkan.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+class Main {
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	void initVulkan() {
+	}
+
+	void mainLoop() {
+	}
+
+	void cleanup() {
+	}
+};
+
+int main() {
+	Main app;
+
+	try {
+		app.run();
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
