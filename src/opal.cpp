b2e640f2cee1a39ea7c07b46c82c8e1410119c5a
jacobcoughenour <jacob15545@yahoo.com>
2020-10-25 23:56:23 -0400

    things are working again

---
+++ src/opal.cpp
@@ -0,0 +1,11 @@
+﻿#include "opal.h"
+
+
+void Opal::setup(
+		const vk::Instance &instance,
+		const vk::Device &device,
+		const vk::PhysicalDevice &physicalDevice,
+		uint32_t graphicsQueueIndex) {
+	AppBase::setup(instance, device, physicalDevice, graphicsQueueIndex);
+}
+
---
+++ src/opal.h
@@ -0,0 +1,16 @@
+﻿#ifndef OPAL_OPAL_H
+#define OPAL_OPAL_H
+
+#include <nvvk/appbase_vkpp.hpp>
+
+
+class Opal : public nvvk::AppBase {
+
+public:
+
+	void setup(const vk::Instance &instance, const vk::Device &device, const vk::PhysicalDevice &physicalDevice, uint32_t graphicsQueueIndex) override;
+
+
+};
+
+#endif //OPAL_OPAL_H
